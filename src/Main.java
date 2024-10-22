ea36a1f3456bccede8af2b3bfc7da24adeaee951
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2024-10-22 10:19:30 +0200

    Forgor commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tamagotchi tamagotchi = new Tamagotchi();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Tamagotchi blyat");
+
+        System.out.println("Choose a name for tamagotchi, davai");
+        tamagotchi.Name = scanner.nextLine();
+
+        System.out.println("-_-  " + tamagotchi.Name + "? Really? Ok then.");
+
+        while (tamagotchi.GetAlive()) {
+            tamagotchi.PrintStats();
+            System.out.println("");
+            tamagotchi.choices();
+            String choice = scanner.nextLine();
+            if (choice == "1"){
+                tamagotchi.Hi();
+            }
+            else if (choice == "2"){
+                tamagotchi.feed();
+            }
+            else if (choice == "3"){
+                System.out.println("What word do you wanna teach them?");
+                String word = scanner.nextLine();
+                tamagotchi.Teach(word);
+            }
+            else {
+                System.out.println("Staring intensly at " + tamagotchi.Name + ".");
+            }
+
+
+            tamagotchi.Tick();
+            System.out.println("Press [ENTER] to continue.");
+            scanner.nextLine();
+        }
+
+        System.out.println("You really let " + tamagotchi.Name + " die that easy huh? Not cool.");
+        System.out.println("Press [ENTER] and leave me. Alone.");
     }
 }
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Tamagotchi {
+
+    private int hunger = 0;
+    private int boredom = 0;
+    private ArrayList<String> words = new ArrayList<String>();
+    private boolean isAlive;
+    public String Name;
+    Random rand = new Random();
+
+    public Tamagotchi() {
+        isAlive = true;
+        words.add("blyat");
+    }
+
+
+    public void Tick() {
+        hunger++;
+        boredom++;
+        if (hunger > 10 || boredom > 10) {
+            isAlive = false;
+        }
+    }
+
+    public void choices() {
+        System.out.println("What's the next move?");
+        System.out.println("1. Talk to whatever that thing is?");
+        System.out.println("2. Feed the bastard?");
+        System.out.println("3. Educate your creature with a new word?");
+        System.out.println("4. Stay silent.");
+    }
+
+    public void feed() {
+        System.out.println(Name + " is fed and becomes less hungry.");
+        hunger -= 2;
+        if (hunger < 0) {
+            hunger = 0;
+        }
+    }
+
+    public void Hi() {
+        int wordNum = rand.nextInt(words.size());
+        System.out.println(Name + " says: " + words.get(wordNum));
+        ReduceBoredom();
+    }
+
+    public void Teach(String word) {
+        System.out.println("You teach " + Name + "to say: " + word);
+        words.add(word);
+        ReduceBoredom();
+    }
+
+    public void PrintStats() {
+        System.out.println("Tamagotchi name: " + Name + " || Level of hunger: " + hunger + " || Level of boredom: " + boredom + " || Known words: " + words.size() + "words");
+    }
+
+    public boolean GetAlive() {
+        return isAlive;
+    }
+
+    private void ReduceBoredom() {
+        System.out.println("You play with " + Name + ", reducing its boredom.");
+        boredom -= 2;
+        if (boredom < 0) {
+            boredom = 0;
+        }
+    }
 }
